f4e0a061b12cdc80832b937488e33760d67a8ffa
Lindsey C <lmcarboneau1654@eagle.fgcu.edu>
2014-04-17 16:18:14 -0400

    Added .php files for: temporary passwords to test login, dashboard with 'security', and a more convincing login procedure.  Also updated the login HTML file, so there's no URL direct to the dashboard page anymore.

---
+++ passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+$USERS["admin"] = ["adminpw"];
+
+function check_logged() { 
+	global $_SESSION, $USERS;
+	if (!array_key_exists($_SESSION["logged"],$USERS)) {
+		header("Location: login.php");
+	};
+};
+?>
