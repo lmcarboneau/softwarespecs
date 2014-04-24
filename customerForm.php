620ca003f54ad5c686efc63fab08b498a1455270
Lindsey C <lmcarboneau1654@eagle.fgcu.edu>
2014-04-24 01:47:23 -0700

    Updated to include an actual form, again sans the php for the most part.  The php from the original file is currently commented out, but still there in case it's still usable/important.
    If Stetson wants to change the columns, that's up to him, but I officially have no patience for it any more.  I think the right-hand side is a little too far to the right, but what do I know?

--- customerForm.php
+++ customerForm.php
@@ -56,20 +56,23 @@ require_once("php/technicians.class.php");
 		<h3><img src="img/ipslogosmall.jpg"></h3>
         
 		<ul class="nav nav-justified">
-          <li><a href="#">Dashboard</a></li>
+          <li><a href="dashboard.php">Dashboard</a></li>
           <li class="active"><a href="customers.php">Customers</a></li>
-          <li><a href="dashboard.php">Technicians</a></li>
-          <li><a href="#">Replacements</a></li>
+          <li><a href="technicians.php">Technicians</a></li>
+          <li><a href="replacements.php">Replacements</a></li>
         </ul>
 		
       </div>
 
-    <div class="page-header">  
-	  <h1></h1>
-	</div>  
+    <div class="page-header">    <!-- MORE PHP GOES HERE. Haven't gotten to it yet. -->
+	  <p style="float:right;"><a href="customers.php"><button type="button" class="btn btn-success">Save Changes</button></a></p>
+	  <h2>Customer Form </h2>  <!-- Should say something like "new customer" or their name if it's an edit -->
+	  <!-- also, most of this probably shouldn't show if the PHP throws the "no customer selected" thing -->
+	</div> 
 	<div class="row" >  
-		<?php
-			if (isset($_GET['id'])){
+		
+		<?php 
+			/* if (isset($_GET['id'])){
 				$customerID = $_GET['id'];
 				echo "Customer ID: ".$customerID;
 			}else if (isset($_POST['id'])){
@@ -77,8 +80,109 @@ require_once("php/technicians.class.php");
 				echo "Customer ID: ".$customerID;
 			}else{
 				echo "No customer selected to edit.";
-			}
-		?>
+			} */
+			
+			// commented because I can't see what I'm doing when it's complaining. And what I'm doing shouldn't 
+			// display if the php is complaining anyway.
+		// ?> 
+	
+	<!-- Main body, with graphs and such -->
+	
+		<div class="col-lg-6">
+				<form class="form-horizontal" role="form">
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Customer Name</label>
+						<div class="col-sm-5"> 
+							<input class="form-control">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Technician</label>
+						<!-- PHP GOES HERE -->
+						<div class="col-sm-5">
+							<select class="col-sm-5 form-control">
+								<option>Select...</option>
+								<option>Bill</option>
+								<option>Jeff</option>
+							</select>
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Address</label>
+						<div class="col-sm-5"> 
+							<input class="form-control">
+						</div>
+						<div class="col-sm-5 col-sm-offset-4"> 
+							<input class="form-control">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">City</label>
+						<div class="col-sm-5"> 
+							<input class="form-control">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">State</label>
+						<div class="col-sm-5">
+							<select class="col-sm-5 form-control">
+								<option>Select...</option>
+								<option>Florida</option>
+								<option>Other</option>
+							</select>
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Zip</label>
+						<div class="col-sm-5"> 
+							<input class="form-control">
+						</div>
+					</div>
+					
+					
+				</form>
+			</div>
+			
+			<div class="col-lg-6">
+				<form class="form-horizontal" role="form">
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Contact Name</label>
+						<div class="col-sm-5"> 
+							<input class="form-control">
+						</div>
+					</div>
+					
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Phone</label>
+						<div class="col-sm-5"> 
+							<input class="form-control" placeholder="(555) 555-5555">
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label class="col-sm-4 control-label">Email</label>
+						<div class="col-sm-5"> 
+							<input class="form-control" placeholder="name@example.com">
+						</div>
+					</div>
+					
+					<div class="col-md-6 col-md-offset-4">
+						<textarea class="form-control" rows="4" placeholder="Comments"></textarea>
+					</div>	
+					
+					
+				</form>
+			</div>
+			
+	
 	</div> <!-- ends panel 'row' div -->
 
 
