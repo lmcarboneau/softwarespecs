bbec39900e4a43f9ae974cf8bc04075a5868b6da
Austin Hughes <ahughes02@live.com>
2014-09-16 15:11:25 -0400

    connection fix

--- php/config.inc.php
+++ php/config.inc.php
@@ -15,7 +15,7 @@ require_once('config.class.php');
  * Write settings to the config
  */
 Config::write('hostname', 'us-cdbr-azure-central-a.cloudapp.net');
-Config::write('database', 'ipstest');
+Config::write('database', 'multitrackdb');
 Config::write('username', 'b16d99ad59ac38');
 Config::write('password', '4b984b87');
 Config::write('drivers', array(PDO::ATTR_PERSISTENT => false));
